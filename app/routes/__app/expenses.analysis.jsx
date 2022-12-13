744d161196f9daa5dfdb1a5e555ab648a80b7b2b
Steven Duncan <Steven.Duncan@paccar.com>
2022-12-13 14:31:09 -0800

    initial commit

---
+++ app/routes/__app/expenses.analysis.jsx
@@ -0,0 +1,26 @@
+import Chart from "~/components/expenses/Chart";
+import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export default function ExpensesAnalysisPage() {
+    return (
+      <main>
+        <Chart expenses={DUMMY_EXPENSES} />
+        <ExpenseStatistics expenses={DUMMY_EXPENSES} />
+      </main>  
+    );
+}
---
+++ app/routes/__app/expenses.jsx
@@ -0,0 +1,39 @@
+import { Link, Outlet } from "@remix-run/react";
+import { FaPlus, FaDownload } from "react-icons/fa";
+import ExpensesList from "~/components/expenses/ExpensesList";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+export default function ExpensesLayout() {
+  return (
+    <>
+      <Outlet />
+      <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
+        <ExpensesList expenses={DUMMY_EXPENSES} />
+      </main>
+    </>
+  );
+}
